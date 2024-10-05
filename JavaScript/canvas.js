a181982e6937d7d96996c6953fa7bd44086a9128
La Sociedad Wiki <135772355+zomjugos@users.noreply.github.com>
2024-10-05 09:26:42 -0300

    Add files via upload

--- JavaScript/canvas.js
+++ JavaScript/canvas.js
@@ -1,17 +1,22 @@
- function dibujo(){
-    var acepta = retornarRecuadro("canvas");
-    if(acepta){
-        acepta.font = "bold 1.5625rem Sans-serif";
-        acepta.textAlign = "left";
-        acepta.fillText("La sociedad Fandom Wiki", 10, 30);
-    }
- }
- function retornarRecuadro(x){
-    var canvas = document.getElementById(x);
-    if(canvas.getContext){
-        var acepta = canvas.getContext("2d");
-        return acepta;
-    }else{
-        return false;
-    }
- }
+const typed = new Typed('.typed', {
+    strings: ['Dylan',
+        'Santi',
+        'Elias',
+        'Luca (mejor conocido solo como Quiroz)',
+        'el mono (me olvide su nombre)',
+        'Mauri',
+        'Fede',
+        'vida'
+    ],
+    typedSpeed: 75,
+    startDelay: 300,
+    typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+});
